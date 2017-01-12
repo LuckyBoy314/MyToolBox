f3622ddbce59605632293412aa18758ad9a6ff14
LuckyBoy314 <zhufeng314@163.comm>
2017-01-12 10:41:32 +0800

    增加批量删除指定文件名的函数

--- walk_directory.py
+++ walk_directory.py
@@ -19,5 +19,18 @@ def FileContentReplace(directory,origin,replace):
 			f.write(line.replace(origin,replace))    
 		f.close()
 
+def FileDelete(directory,tag):
+	"删除directory中以tag开头的文件"
+	assert os.path.isdir(directory)
+	results = []
+	for root, dirs, files in os.walk(directory, topdown=True):
+		for fl in files:
+			results.append(os.path.join(root, fl))
+
+	for each_file in results:
+		filename =  os.path.basename(each_file)
+		if filename.startswith(tag):
+			os.remove(each_file)
+
 if __name__ == "__main__":
-	FileContentReplace("Z:/EC细","S","Z")
+	FileDelete("D:/T639_HR","17011008")
